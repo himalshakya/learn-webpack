45bde06203078b73063b7b082aabb77b599a35eb
Himal Shakya <shakyahimal1@gmail.com>
2020-07-30 11:14:21 -0400

    Part 6

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'main.[contentHash].js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
@@ -13,4 +14,7 @@ module.exports = {
       use: ['style-loader', 'css-loader', 'sass-loader'],
     }],
   },
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/template.html',
+  })],
 };
