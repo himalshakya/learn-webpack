3dfa3a9a0b323b57ed2b435830cefbc9343c7de3
Himal Shakya <shakyahimal1@gmail.com>
2020-07-30 01:10:33 -0400

    Adding SCSS, bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import run from './app/app';
 import AlertService from './app/alert.service';
 import ComponentService from './app/component.service';
+import './main.scss';
 
 run(new AlertService(), new ComponentService());
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,12 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  }
-}
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [{
+      test: /\.scss$/i,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+    }],
+  },
+};
